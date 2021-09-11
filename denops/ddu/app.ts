19e4aedcf65ddb124e0c75e39414704c86b3011c
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-09-11 14:52:58 +0900

    Add denops module

---
+++ denops/ddu/app.ts
@@ -0,0 +1,10 @@
+import { Denops, batch, vars } from "./deps.ts";
+
+export async function main(denops: Denops) {
+  denops.dispatcher = {
+  };
+
+  await batch(denops, async (denops: Denops) => {
+    await vars.g.set(denops, "ddu#_initialized", 1);
+  });
+}
---
+++ denops/ddu/deps.ts
@@ -0,0 +1,18 @@
+export type { Denops } from "https://deno.land/x/denops_std@v1.10.0/mod.ts#^";
+export { execute } from "https://deno.land/x/denops_std@v1.10.0/helper/mod.ts#^";
+export {
+  batch,
+  gather,
+} from "https://deno.land/x/denops_std@v1.10.0/batch/mod.ts#^";
+export * as op from "https://deno.land/x/denops_std@v1.10.0/option/mod.ts#^";
+export * as fn from "https://deno.land/x/denops_std@v1.10.0/function/mod.ts#^";
+export * as vars from "https://deno.land/x/denops_std@v1.10.0/variable/mod.ts#^";
+export * as autocmd from "https://deno.land/x/denops_std@v1.10.0/autocmd/mod.ts#^";
+export { ensureObject } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts#^";
+export { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts#^";
+export { parse, toFileUrl } from "https://deno.land/std@0.106.0/path/mod.ts#^";
+export {
+  deadline,
+  DeadlineError,
+} from "https://deno.land/std@0.106.0/async/mod.ts#^";
+export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.0/response_waiter.ts#^";
