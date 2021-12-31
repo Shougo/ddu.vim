98ddd4b6dd3a660d3301059db2170302cb22525c
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-12-31 17:20:44 +0900

    Fix redraw() implementation

--- denops/@ddu-uis/std.ts
+++ denops/@ddu-uis/std.ts
@@ -1,16 +1,24 @@
-import { BaseUi, DduItem } from "../ddu/types.ts";
+import { BaseUi, DduItem, UiOptions } from "../ddu/types.ts";
 import { Denops, fn, vars } from "../ddu/deps.ts";
 
 type Params = Record<never, never>;
 
 export class Ui extends BaseUi<Params> {
   async redraw(args: {
-    denops: Denops;
-    items: DduItem[];
+    denops: Denops,
+    uiOptions: UiOptions,
+    items: DduItem[],
   }): Promise<void> {
-    // Initialize buffer
-    const bufnr = await fn.bufadd(args.denops, "ddu-std-bufferName");
-    await fn.bufload(args.denops, bufnr);
+    const bufferName = `ddu-std-${args.uiOptions.bufferName}`;
+    let bufnr;
+    if (await fn.bufexists(args.denops, bufferName)) {
+      bufnr = await fn.bufnr(args.denops, bufferName);
+    } else {
+      // Initialize buffer
+      bufnr = await fn.bufadd(args.denops, bufferName);
+      await fn.bufload(args.denops, bufnr);
+    }
+
     await fn.setbufvar(args.denops, bufnr, "&modifiable", 1);
     await fn.setbufline(args.denops, bufnr, 1, args.items.map((c) => c.word));
     await fn.setbufvar(args.denops, bufnr, "&modifiable", 0);
