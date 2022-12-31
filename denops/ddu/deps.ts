48a90b7daa0f9895f185b6890e54548e11428699
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-12-31 16:12:09 +0900

    Update

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -1,16 +1,16 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.10.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.12.0/mod.ts";
 export {
   echo,
   execute,
-} from "https://deno.land/x/denops_std@v3.10.0/helper/mod.ts";
+} from "https://deno.land/x/denops_std@v3.12.0/helper/mod.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v3.10.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v3.10.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.10.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.10.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.10.0/autocmd/mod.ts";
+} from "https://deno.land/x/denops_std@v3.12.0/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v3.12.0/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.12.0/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.12.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts";
 export {
   ensureArray,
   ensureObject,
@@ -19,12 +19,12 @@ export {
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.167.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.167.0/path/mod.ts";
+} from "https://deno.land/std@0.170.0/testing/asserts.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.170.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.167.0/async/mod.ts";
+} from "https://deno.land/std@0.170.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.0/response_waiter.ts";
 export { Lock } from "https://deno.land/x/async@v1.2.0/mod.ts";
-export { basename } from "https://deno.land/std@0.167.0/path/mod.ts";
+export { basename } from "https://deno.land/std@0.170.0/path/mod.ts";
