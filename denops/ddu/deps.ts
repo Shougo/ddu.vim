a1e251198fc458a1d70f76505f5c3f125d5bb8aa
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-05-23 07:42:37 +0900

    Update deps

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -1,16 +1,16 @@
-export type { Denops } from "https://deno.land/x/denops_std@v4.3.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
 export {
   echo,
   execute,
-} from "https://deno.land/x/denops_std@v4.3.0/helper/mod.ts";
+} from "https://deno.land/x/denops_std@v5.0.0/helper/mod.ts";
 export {
   batch,
   collect,
-} from "https://deno.land/x/denops_std@v4.3.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v4.3.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v4.3.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v4.3.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v4.3.0/autocmd/mod.ts";
+} from "https://deno.land/x/denops_std@v5.0.0/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v5.0.0/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v5.0.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v5.0.0/autocmd/mod.ts";
 export {
   ensureArray,
   ensureNumber,
@@ -20,16 +20,16 @@ export {
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.187.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.187.0/path/mod.ts";
+} from "https://deno.land/std@0.188.0/testing/asserts.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.188.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.187.0/async/mod.ts";
+} from "https://deno.land/std@0.188.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "https://deno.land/x/async@v2.0.2/mod.ts";
 export {
   basename,
   dirname,
   SEP as pathsep,
-} from "https://deno.land/std@0.187.0/path/mod.ts";
+} from "https://deno.land/std@0.188.0/path/mod.ts";
