6417de1294bd23e666884dfc794fd7fb95313017
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-07-29 10:53:50 +0900

    Update deps

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -11,21 +11,21 @@ export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts";
-export { ensure, is } from "https://deno.land/x/unknownutil@v3.2.0/mod.ts";
+export { ensure, is } from "https://deno.land/x/unknownutil@v3.4.0/mod.ts";
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.194.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.194.0/path/mod.ts";
+} from "https://deno.land/std@0.196.0/assert/mod.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.196.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.194.0/async/mod.ts";
+} from "https://deno.land/std@0.196.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "https://deno.land/x/async@v2.0.2/mod.ts";
 export {
   basename,
   dirname,
   SEP as pathsep,
-} from "https://deno.land/std@0.194.0/path/mod.ts";
+} from "https://deno.land/std@0.196.0/path/mod.ts";
 export { deferred } from "https://deno.land/std@0.196.0/async/deferred.ts";
