9e20c2ca1541ffdc1283088c4264a9b0941fdc48
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-04-12 21:27:36 +0900

    Update deps

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -15,20 +15,20 @@ export {
   ensure,
   is,
   maybe,
-} from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
+} from "https://deno.land/x/unknownutil@v3.17.2/mod.ts";
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.221.0/assert/mod.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.221.0/path/mod.ts";
+} from "https://deno.land/std@0.222.1/assert/mod.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.222.1/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.221.0/async/mod.ts";
+} from "https://deno.land/std@0.222.1/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "https://deno.land/x/async@v2.1.0/mod.ts";
 export {
   basename,
   dirname,
   SEPARATOR as pathsep,
-} from "https://deno.land/std@0.221.0/path/mod.ts";
+} from "https://deno.land/std@0.222.1/path/mod.ts";
