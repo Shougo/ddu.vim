4e5dd65c72c9fda6ea66a1f7e0e547d139a5d5f3
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-05-04 11:41:59 +0900

    Use JSR modules

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -11,24 +11,21 @@ export * as op from "https://deno.land/x/denops_std@v6.4.0/option/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
+
 export {
   ensure,
   is,
   maybe,
 } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
-export {
-  assertEquals,
-  equal,
-} from "https://deno.land/std@0.224.0/assert/mod.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.224.0/path/mod.ts";
-export {
-  deadline,
-  DeadlineError,
-} from "https://deno.land/std@0.224.0/async/mod.ts";
-export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
-export { Lock } from "https://deno.land/x/async@v2.1.0/mod.ts";
+export { assertEquals, equal } from "jsr:@std/assert@0.224.0";
 export {
   basename,
   dirname,
+  parse,
   SEPARATOR as pathsep,
-} from "https://deno.land/std@0.224.0/path/mod.ts";
+  toFileUrl,
+} from "jsr:@std/path@0.224.0";
+export { deadline, DeadlineError } from "jsr:@std/async@0.224.0";
+
+export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
+export { Lock } from "https://deno.land/x/async@v2.1.0/mod.ts";
