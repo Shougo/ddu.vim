560aef72ea60a566c655cb217454fcce86edffd5
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-05-13 08:19:20 +0900

    Use JSR unknownutil

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -12,11 +12,6 @@ export * as fn from "https://deno.land/x/denops_std@v6.4.2/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v6.4.2/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v6.4.2/autocmd/mod.ts";
 
-export {
-  ensure,
-  is,
-  maybe,
-} from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
 export { assertEquals, equal } from "jsr:@std/assert@0.225.1";
 export {
   basename,
@@ -27,5 +22,10 @@ export {
 } from "jsr:@std/path@0.224.0";
 export { deadline, DeadlineError } from "jsr:@std/async@0.224.0";
 
+export {
+  ensure,
+  is,
+  maybe,
+} from "jsr:@core/unknownutil@3.18.0";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "jsr:@lambdalisue/async@2.1.1";
