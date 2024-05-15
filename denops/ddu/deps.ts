6fc30dd18f7b43327e8d4c375b566cc47938a529
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-05-16 08:31:56 +0900

    Update unknownutil

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -25,6 +25,6 @@ export {
 } from "jsr:@std/path@0.224.0";
 export { deadline, DeadlineError } from "jsr:@std/async@0.224.0";
 
-export { ensure, is, maybe } from "jsr:@core/unknownutil@3.18.0";
+export { ensure, is, maybe } from "jsr:@core/unknownutil@3.18.1";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "jsr:@lambdalisue/async@2.1.1";
