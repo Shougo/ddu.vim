eb5c5d275fdd41127f1251395a47b3b589b6f865
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-07-31 13:08:24 +0900

    Update deps

--- denops/ddu/deps.ts
+++ denops/ddu/deps.ts
@@ -1,19 +1,19 @@
-export type { Denops, Entrypoint } from "jsr:@denops/std@7.0.0";
-export { echo, execute } from "jsr:@denops/std@7.0.0/helper";
-export { batch, collect } from "jsr:@denops/std@7.0.0/batch";
-export * as op from "jsr:@denops/std@7.0.0/option";
-export * as fn from "jsr:@denops/std@7.0.0/function";
-export * as vars from "jsr:@denops/std@7.0.0/variable";
-export * as autocmd from "jsr:@denops/std@7.0.0/autocmd";
+export type { Denops, Entrypoint } from "jsr:@denops/std@7.0.1";
+export { echo, execute } from "jsr:@denops/std@7.0.1/helper";
+export { batch, collect } from "jsr:@denops/std@7.0.1/batch";
+export * as op from "jsr:@denops/std@7.0.1/option";
+export * as fn from "jsr:@denops/std@7.0.1/function";
+export * as vars from "jsr:@denops/std@7.0.1/variable";
+export * as autocmd from "jsr:@denops/std@7.0.1/autocmd";
 
-export { assertEquals, equal } from "jsr:@std/assert@1.0.0";
+export { assertEquals, equal } from "jsr:@std/assert@1.0.1";
 export {
   basename,
   dirname,
   parse,
   SEPARATOR as pathsep,
   toFileUrl,
-} from "jsr:@std/path@1.0.1";
+} from "jsr:@std/path@1.0.2";
 
 export { ensure, is, maybe } from "jsr:@core/unknownutil@3.18.1";
 export { Lock } from "jsr:@lambdalisue/async@2.1.1";
