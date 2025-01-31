f14d60bee74a6e115e96a18853243fc497340d91
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-01-31 12:13:43 +0900

    Add base kind

---
+++ denops/@ddu-kinds/base.ts
@@ -0,0 +1,15 @@
+import { type ActionArguments, ActionFlags } from "../ddu/types.ts";
+import { BaseKind } from "../ddu/base/kind.ts";
+
+type Params = Record<string, never>;
+
+export class Kind extends BaseKind<Params> {
+  override actions: Record<
+    string,
+    (args: ActionArguments<Params>) => Promise<ActionFlags>
+  > = {};
+
+  override params(): Params {
+    return {};
+  }
+}
