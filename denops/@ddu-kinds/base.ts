44ec3d5b801c3b93eb36b2dd602905f4f397d0e0
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-04-14 09:39:35 +0900

    Update base

--- denops/@ddu-kinds/base.ts
+++ denops/@ddu-kinds/base.ts
@@ -1,13 +1,10 @@
-import type { ActionArguments, ActionFlags } from "../ddu/types.ts";
+import type { Actions } from "../ddu/types.ts";
 import { BaseKind } from "../ddu/base/kind.ts";
 
 type Params = Record<string, never>;
 
 export class Kind extends BaseKind<Params> {
-  override actions: Record<
-    string,
-    (args: ActionArguments<Params>) => Promise<ActionFlags>
-  > = {};
+  override actions: Actions<Params> = {};
 
   override params(): Params {
     return {};
