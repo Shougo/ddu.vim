8f987c8123193f6df6f407b78c28a926b1424265
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-07-31 12:01:48 +0900

    Add import map support for config file

--- denops/ddu/utils.ts
+++ denops/ddu/utils.ts
@@ -1,6 +1,15 @@
 import type { Denops } from "@denops/std";
 import { SEPARATOR as pathsep } from "@std/path/constants";
 import type { TreePath } from "./types.ts";
+import {
+  type ImportMap,
+  ImportMapImporter,
+  loadImportMap,
+} from "@lambdalisue/import-map-importer";
+import { toFileUrl } from "@std/path/to-file-url";
+import { fromFileUrl } from "@std/path/from-file-url";
+import { join } from "@std/path/join";
+import { dirname } from "@std/path/dirname";
 
 export async function printError(
   denops: Denops,
@@ -67,4 +76,49 @@ export function convertUserString<T>(
   user: string | T,
 ): T | { name: string | (T & string) } {
   return typeof user === "string" ? { name: user } : user;
+}
+
+export async function tryLoadImportMap(
+  script: string,
+): Promise<ImportMap | undefined> {
+  if (script.startsWith("http://") || script.startsWith("https://")) {
+    // We cannot load import maps for remote scripts
+    return undefined;
+  }
+  const PATTERNS = [
+    "deno.json",
+    "deno.jsonc",
+    "import_map.json",
+    "import_map.jsonc",
+  ];
+  // Convert file URL to path for file operations
+  const scriptPath = script.startsWith("file://")
+    ? fromFileUrl(new URL(script))
+    : script;
+  const parentDir = dirname(scriptPath);
+  for (const pattern of PATTERNS) {
+    const importMapPath = join(parentDir, pattern);
+    try {
+      return await loadImportMap(importMapPath);
+    } catch (err: unknown) {
+      if (err instanceof Deno.errors.NotFound) {
+        // Ignore NotFound errors and try the next pattern
+        continue;
+      }
+      throw err; // Rethrow other errors
+    }
+  }
+  return undefined;
+}
+
+export async function importPlugin(path: string): Promise<unknown> {
+  const suffix = performance.now();
+  const url = toFileUrl(path).href;
+  const importMap = await tryLoadImportMap(path);
+  if (importMap) {
+    const importer = new ImportMapImporter(importMap);
+    return await importer.import(`${url}#${suffix}`);
+  } else {
+    return await import(`${url}#${suffix}`);
+  }
 }
